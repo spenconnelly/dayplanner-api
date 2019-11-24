0d8b1c14ce2793d917d4ab28bdb05028f1d5eb9b
spencerconnelly <spenconnelly@gmail.com>
2019-11-24 11:46:32 -0500

    Add eslint dev dependency.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "semi": 2,
+        "no-console": 0
+    }
+};
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
+app.use('/api/v1', router);
 
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080!');
+});
